1d65da74595a454ce5b2796c670eb32b84f5bc8f
huxf <914036951@qq.com>
2019-06-17 17:25:18 +0800

    2019-06-17

--- app/src/main/java/com/smartbus/heze/ApiAddress.java
+++ app/src/main/java/com/smartbus/heze/ApiAddress.java
@@ -6,13 +6,13 @@ package com.smartbus.heze;
 
 public class ApiAddress {
     //李志浩
-    public final static String mainApi = "http://192.168.2.114:80/joffice21/" ;
+//    public final static String mainApi = "http://192.168.2.114:80/joffice21/" ;
     //川川
 //    public final static String mainApi = "http://192.168.2.126:8082/joffice21/" ;
     //孙琦
 //    public final static String mainApi = "http://192.168.2.154:8080/joffice21/" ;
     //正式库
-//    public final static String mainApi = "http://120.192.74.58:8080/joffice/" ;
+    public final static String mainApi = "http://120.192.74.58:8080/joffice/" ;
 //    public final static String mainApi = "http://192.168.2.117:8080/joffice21/" ;
 
     /***********************首页*******************************/
